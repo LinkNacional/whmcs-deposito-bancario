eb0db21c5e6e33d470c3e537c1794ef33f72c534
Bruno Ferreira <ferreira.bruno@linknacional.com>
2023-02-13 09:25:49 -0300

    feat: implementar campos de dados para depósito

---
+++ src/modules/gateways/lkn_deposito_bancario.php
@@ -0,0 +1,93 @@
+<?php
+
+use Smarty;
+
+if (!defined('WHMCS')) {
+    die;
+}
+
+/**
+ * @return array
+ */
+function lkn_deposito_bancario_MetaData()
+{
+    return [
+        'DisplayName' => 'Depósito Bancário',
+        'APIVersion' => '1.1', // Use API Version 1.1
+        'DisableLocalCreditCardInput' => true,
+        'TokenisedStorage' => false,
+    ];
+}
+
+/**
+ * @return array
+ */
+function lkn_deposito_bancario_config()
+{
+    return [
+        'FriendlyName' => [
+            'Type' => 'System',
+            'Value' => 'Depósito Bancário',
+        ],
+
+        'cnpj' => [
+            'FriendlyName' => 'CNPJ',
+            'Description' => '',
+            'Type' => 'text',
+            'Size' => '25',
+        ],
+
+        'bank' => [
+            'FriendlyName' => 'Banco',
+            'Description' => '',
+            'Type' => 'text',
+            'Size' => '25',
+        ],
+
+        'holder' => [
+            'FriendlyName' => 'Titular',
+            'Description' => '',
+            'Type' => 'text',
+            'Size' => '25',
+        ],
+
+        'agency_number' => [
+            'FriendlyName' => 'Agência',
+            'Description' => '',
+            'Type' => 'text',
+            'Size' => '25',
+        ],
+
+        'account_number' => [
+            'FriendlyName' => 'Conta',
+            'Description' => '',
+            'Type' => 'text',
+            'Size' => '25',
+        ]
+    ];
+}
+
+/**
+ * @see https://developers.whmcs.com/payment-gateways/third-party-gateway/
+ *
+ * @param array $params Payment Gateway Module Parameters
+ *
+ * @return string
+ */
+function lkn_deposito_bancario_link($params)
+{
+    $cnpj = $params['cnpj'];
+    $bank = $params['bank'];
+    $holder = $params['holder'];
+    $agencyNumber = $params['agency_number'];
+    $accountNumber = $params['account_number'];
+
+    $smarty = new Smarty();
+    $smarty->assign('cnpj', $cnpj);
+    $smarty->assign('bank', $bank);
+    $smarty->assign('holder', $holder);
+    $smarty->assign('agencyNumber', $agencyNumber);
+    $smarty->assign('accountNumber', $accountNumber);
+
+    return $smarty->fetch(__DIR__ . '/lkn_deposito_bancario/templates/deposit_data_display.tpl');
+}
---
+++ src/modules/gateways/lkn_deposito_bancario/templates/deposit_data_display.tpl
@@ -0,0 +1,27 @@
+<div style="text-align: left; margin: 0 auto 0; width: max-content;">
+    <div style="margin-bottom: 15px;">
+        <span style="display: inline-block; width: 68px; font-weight: bold;">Titular:</span> {$holder}
+    </div>
+
+    <div style="margin-bottom: 15px;">
+        <span style="display: inline-block; width: 68px; font-weight: bold;">CNPJ:</span> {$cnpj}
+    </div>
+
+    <div style="margin-bottom: 15px;">
+        <span style="display: inline-block; width: 68px; font-weight: bold;">Banco:</span> {$bank}
+    </div>
+
+    <div style="margin-bottom: 15px;">
+        <span style="display: inline-block; width: 68px; font-weight: bold;">Conta:</span> {$accountNumber}
+    </div>
+
+    <div style="margin-bottom: 15px;">
+        <span style="display: inline-block; width: 68px; font-weight: bold;">Agência:</span> {$agencyNumber}
+    </div>
+
+</div>
+
+<script type="text/javascript">
+    // Hiddes pay button.
+    document.querySelector('form[name="paymentfrm"] button').style.display = 'none'
+</script>
